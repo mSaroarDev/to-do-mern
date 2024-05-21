8bc62c07e5d00342866c750f7e90ec69e65f8c7a
Muhammad Saroar <136387545+mSaroarDev@users.noreply.github.com>
2024-05-21 22:12:39 +0600

    cookie policy changed

--- server.js
+++ server.js
@@ -25,6 +25,18 @@ app.use(
   })
 );
 
+app.all("*", function (req, res, next) {
+  const origin = cors.origin.includes(req.header("origin").toLowerCase())
+    ? req.headers.origin
+    : cors.default;
+  res.header("Access-Control-Allow-Origin", origin);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // database setup
 mongoose
   .connect(process.env.MONGODB_URL)
